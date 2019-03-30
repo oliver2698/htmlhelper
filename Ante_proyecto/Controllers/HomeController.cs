361ebf5e9eac97ce75a7a4c18a470317ac0f6ce1
oliver2698 <oliver2698@users.noreply.github.com>
2019-03-30 19:45:08 -0400

    htmlhelper
    tarea

---
+++ Ante_proyecto/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Ante_proyecto.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ante_proyecto.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Formulario()
+        {
+         
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FormSubmit(Formulario Formulario)
+        {
+            if (ModelState.IsValid)
+            {
+                string Cedula = Formulario.Cedula.ToString();
+                string Nombre = Formulario.Nombre;
+                string Apellido = Formulario.Apellido;
+                string Genero = Formulario.Genero;
+                string Correo = Formulario.Correo;
+                string Estado = Formulario.Estado_civil;
+                string Edad = Formulario.Edad.ToString();
+                string Telefono = Formulario.Telefono;
+                bool Hobby = Formulario.Hobby;
+            }
+
+            return View("FormSubmit", Formulario);
+        }
+    }
+}
---
+++ Ante_proyecto/Models/Formulario.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Ante_proyecto.Models
+{
+    public class Formulario
+    {
+        [DisplayName("Cedula")]
+        [Required]
+        public int Cedula { get; set; }
+        [DisplayName("Nombre")]
+        [Required]
+        [StringLength(30)]
+        public string Nombre { get; set; }
+        [DisplayName("Apellido")]
+        [Required]
+        [StringLength(30)]
+        public string Apellido { get; set; }
+        [DisplayName("Telefono")]
+        [Required]
+        [StringLength(12)]
+        public string Telefono { get; set; }
+        [DisplayName("Correo")]
+        [Required]
+        [StringLength(30)]
+        [EmailAddress]
+        public string Correo { get; set; }
+        //(Validar el formato de un correo electrónico)
+        [DisplayName("Edad")]
+        [Required]
+        [Range(15, 99, ErrorMessage = "Tiene que ser mayor de 15")]
+        public int Edad { get; set; }
+        //(validar que sea mayor que 15)
+        [DisplayName("Genero")]
+        [Required]
+        public string Genero { get; set; }
+        //(Masculino/Femenino) (DropDownList)
+        [DisplayName("Estado civil")]
+        [Required]
+        public string Estado_civil { get; set; }
+        //(Casado/Soltero/Viudo/Union Libre) (RadioButton)
+        [DisplayName("Hobby")]
+        public bool Hobby { get; set; }
+        //(Checkbox, incluir 4 pasatiempos)
+    }
+}
